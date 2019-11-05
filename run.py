4c1792f9d9543f7963f5e3128b5af40412b8d85e
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-11-05 13:40:13 +0300

    Bug electron starter resolu

--- run.py
+++ run.py
@@ -3,15 +3,11 @@ import eel
 
 options = {
 	'mode' : 'custom',
-	'args' : ['/usr/bin/electron', '.']
+	'args' : ['/usr/lib/electron/electron', '.']
 }
 
 eel.init('view')
 
 
-def main():
-    eel.start('home.html', position=(175,50),  size=(1200,600), options=options)
 
-
-if __name__ == '__main__':
-    main()
+eel.start('home.html', position=(175,50),  options=options)
