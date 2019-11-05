fbcca7d38a510ca42a102daf35f866c3d9a29b4a
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-11-05 13:56:10 +0300

    Taille et structure ajuster

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1200,
+    width: 1000,
     height: 600,
     resizable: false,
     webPreferences: {
