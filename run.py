a06dffa971b94dfabe25b88a7d50da43ef6f084b
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-11-07 22:06:42 +0300

    Validation Branche

--- run.py
+++ run.py
@@ -1,19 +1,45 @@
 import eel
 import sys
+from db.database import database 
+
 
 options = {
 	'mode' : 'custom',
-	'args' : ['/usr/bin/electron4' if sys.platform == 'linux' else '', '.']
+	'args' : ['/usr/bin/electron' if sys.platform == 'linux' else '', '.']
 }
 
 eel.init('view')
 
 @eel.expose
-def test ():
-	print('test')
+def test():
+	'''
+		fonction pour les tests 
+								'''
+	print('hello')
+
 
 def main():
 	eel.start('home.html', position=(135, 35), options=options)
+
+
+@eel.expose
+def login(usr, passwd):
+	try:
+		db = database()
+	except:
+		return "Une erreur s'est produite"
+	else:
+		cursor = db.cursor()
+		cursor.execute('''
+			SELECT 1 FROM Users WHERE username=%s AND PASSWORD=%s
+		''', (usr, passwd))
+
+		
+		if len(cursor.fetchall()) == 1:
+			return True
+		else:
+			return 'Authentification failed' 
+
 
 
 if __name__ == '__main__':
